e0a20e4195331073eb0629474c92220910bdf058
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2022-08-22 08:29:16 +0300

    Configure Env Variables

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -5,7 +5,7 @@ export const baseUrl = "https://bayut.p.rapidapi.com";
 export const fetchApi = async (url) => {
   const { data } = await axios.get(url, {
     headers: {
-      "X-RapidAPI-Key": "559f9bd302mshc5f090bde84e763p13eb56jsn7b5450215a21",
+      "X-RapidAPI-Key": process.env.RAPIDAPI_API_KEY,
       "X-RapidAPI-Host": "bayut.p.rapidapi.com",
     },
   });
